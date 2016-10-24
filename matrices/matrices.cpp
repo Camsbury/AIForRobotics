56f15dfdfdb7631f3d7fdc0ba4ab58d77d026f83
buddha <camsbury7@gmail.com>
2016-10-23 22:49:21 -0400

    Matrices File
    Inspired by Kalman filters from Unit 2. I started the work on creating
    matrix logic for C++. (As an attempt to learn C++, not to actually
    improve the language)

---
+++ matrices/matrices.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <array>
+#include <string>
+
+/**
+ * A template class designed to take 2D Matrices of any size greater
+ * than zero. The matrices can be added, subtracted, multiplied,
+ * transposed, inversed, and shown.
+ */
+
+template <const unsigned _xDim, const unsigned _yDim>
+class Matrix {
+
+    private:
+
+        // The dimensions and values of the Matrix
+        const unsigned xDim = _xDim;
+        const unsigned yDim = _yDim;
+        std::array<double, _xDim*_yDim> values;
+
+    public:
+
+        // A constructor for diagonal matrices with DiagVal
+        Matrix(double diagVal) {
+            values.fill(0);
+            const unsigned diagCount = (xDim < yDim) ? xDim : yDim;
+            for (unsigned index = 0; index != diagCount; index++) {
+                values[index*yDim + index] = diagVal;
+            }
+        }
+
+        // A constructor for input of one array
+        Matrix(std::array<double, _xDim*_yDim> iValues): values(iValues) {}
+        
+        // Getter in X Dimension
+        unsigned getXDim() const {
+            return xDim;
+        }
+
+        // Getter in Y Dimension
+        unsigned getYDim() const {
+            return yDim;
+        }
+
+        // Show the Matrix as a String
+        std::string show() const {
+            std::string display;
+            for (unsigned iX = 0; iX != xDim; ++iX) {
+                for (unsigned iY = 0; iY != yDim; ++iY) {
+                    display += ' ';
+                    display += std::to_string(values[iX*yDim + iY]);
+                    display += ' ';
+                }
+                display += '\n';
+            }
+            return display;
+        }
+};
+
+
+/*
+ * Example Exectution
+ */
+
+int main()
+{
+    Matrix<2, 3> m(1);
+    std::cout << m.show() << std::endl;
+    return 0;
+}
